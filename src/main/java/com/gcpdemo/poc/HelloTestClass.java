26215def686322f398a5954e72ba84637e7086b1
Chandra Shekhar Pandey <shekharmca2005@gmail.com>
2019-07-29 22:55:43 +0530

    logger added

--- src/main/java/com/gcpdemo/poc/HelloTestClass.java
+++ src/main/java/com/gcpdemo/poc/HelloTestClass.java
@@ -1,13 +1,19 @@
 package com.gcpdemo.poc;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class HelloTestClass {
+	private final Logger logger = LoggerFactory.getLogger(HelloTestClass.class);
+	
 	@RequestMapping("/hello")
 public String helloTest()
 {
+	logger.info("This is a test");
 	return "Hello Test";
 }
 }
--- src/main/java/com/gcpdemo/poc/PocApplication.java
+++ src/main/java/com/gcpdemo/poc/PocApplication.java
@@ -2,10 +2,13 @@ package com.gcpdemo.poc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class PocApplication {
 
+	private final Logger logger = LoggerFactory.getLogger(PocApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(PocApplication.class, args);
 	}
